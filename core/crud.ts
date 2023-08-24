1099f9172cd46783698561933af44c7c7adf1172
Gustavo Mello <mographllo@gmail.com>
2023-08-24 13:44:30 -0300

    created interface for to-do

--- core/crud.ts
+++ core/crud.ts
@@ -3,9 +3,31 @@ const DB_FILE_PATH = './core/db'
 
 console.log('CRUD')
 
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
 function create(content: string) {
-  fs.writeFileSync(DB_FILE_PATH, content)
+  const todo: Todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false
+  }
+
+  const todos = [
+    todo
+  ]
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos}, null, 2))
   return content
 }
 
-create('Crud2')
+function read() {
+  const db = fs.readFileSync(DB_FILE_PATH, "utf-8")
+  return db
+}
+
+create('Teste')
+console.log(read())
