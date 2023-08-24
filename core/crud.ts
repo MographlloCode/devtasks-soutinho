4af812f71395754e87adc67c466d8b8b61efb514
Gustavo Mello <mographllo@gmail.com>
2023-08-24 14:39:05 -0300

    created generalist update function

--- core/crud.ts
+++ core/crud.ts
@@ -11,7 +11,7 @@ interface Todo {
   done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
   const todo: Todo = {
     id: uuid(),
     date: new Date().toISOString(),
@@ -25,7 +25,7 @@ function create(content: string) {
   ]
 
   fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos}, null, 2))
-  return content
+  return todo
 }
 
 function read(): Array<Todo> {
@@ -37,6 +37,29 @@ function read(): Array<Todo> {
   return db.todos
 }
 
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+  let updatedTodo;
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    console.log(currentTodo)
+    const isToUpdate = currentTodo.id === id;
+    if(isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partialTodo)
+    }
+  })
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos,}, null, 2))
+  
+  if(!updatedTodo) {
+    throw new Error('Please, provide another ID')
+  }
+
+  return updatedTodo
+
+  
+  console.log('TODOS ATUALIZADAS', todos)
+}
+
 function CLEAR_DB() {
   fs.writeFileSync(DB_FILE_PATH, '')
 }
@@ -44,5 +67,6 @@ function CLEAR_DB() {
 CLEAR_DB()
 create('Teste')
 create('Teste 2')
-create('Teste 3')
-console.log(read())
+const terceiraTodo = create('Teste 3')
+update(terceiraTodo.id, {content: 'Atualizada', done: true})
+// console.log(read())
